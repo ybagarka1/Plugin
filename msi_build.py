369edac4e425509f904a2e12847e4348f0eefaeb
Yash Bagarka <yashbagarka@gmail.com>
2019-01-10 15:41:55 +0000

    enhanced the code

--- msi_build.py
+++ msi_build.py
@@ -2,14 +2,38 @@
 import json
 import requests, zipfile, io 
 from jinja2 import Template
-downloadurl = "http://update.dtitsupport247.net/InstallJunoAgent/Plugin"
-print("sourcing global manifest json file")
-filein = open('globalmanifest.json.template')
-global_manifest_template = Template(filein.read())
-global_manifest_int = json.loads(global_manifest_template.render(downloadurl = downloadurl ))
+import os
+from artifactory import ArtifactoryPath
+from requests.auth import HTTPBasicAuth
+import sys
+from flask import Flask, render_template
 
-for key in global_manifest_int['packages']:
-    print(key['sourceURL'])
-    plugin_req = requests.get(key['sourceURL'])
-    z = zipfile.ZipFile(io.BytesIO(plugin_req.content))
-    z.extractall()
+downloadurl = 'http://update.dtitsupport247.net/InstallJunoAgent/Plugin'
+artifactory_url = 'http://artifact.corp.continuum.net:8081'
+artifactory_password = 'AP49A5SMDpZuQb7e9g7Tn5c45fbUfJkZMzmUSM' 
+
+#artifactory_url = 'http://artifact.corp.continuum.net:8081'
+artifactory_url = os.environ['artifactory_url']
+# artifactory user name
+artifactory_username = 'repluser'
+# artifactoy password
+artifactory_password = os.environ['artifactory_password']
+# repo name for downlaoding
+repo_name = 'int-dev_platform-agent-package'
+
+#run time parameter from jenkins
+build_no = 94
+
+#downloading manifest json template from aritfactory
+
+manifest_json = requests.get("{}/artifactory/{}/{}/globalmanifest.json.template".format(artifactory_url,repo_name,build_no), auth=HTTPBasicAuth(artifactory_username,artifactory_password))
+global_manifest_template = json.loads(manifest_json.content)
+print(global_manifest_template)
+print("sourcing global manifest json file")
+global_manifest_template = Template("{}".format(global_manifest_template))
+global_manifest_int = global_manifest_template.render(downloadurl = downloadurl)
+for i in global_manifest_int['packages']:
+    print(i['packages'])
+#    plugin_req = requests.get(key['sourceURL'])
+#    z = plugin_req.content
+
