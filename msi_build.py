943271de897e2adc580bfcb511b0ec1c6bc6a576
Yash Bagarka <yashbagarka@gmail.com>
2019-01-10 13:09:48 +0000

    script for msi creation

--- msi_build.py
+++ msi_build.py
@@ -1,14 +1,15 @@
 #!/cygdrive/c/Users/yash.bagarka/AppData/Local/Programs/Python/Python36/python
 import json
-import requests
+import requests, zipfile, io 
 from jinja2 import Template
 downloadurl = "http://update.dtitsupport247.net/InstallJunoAgent/Plugin"
 print("sourcing global manifest json file")
-with open('globalmanifest.json.template', 'r') as f:
-    global_manifest_template = json.load(f)
-print(global_manifest_template)
+filein = open('globalmanifest.json.template')
+global_manifest_template = Template(filein.read())
+global_manifest_int = json.loads(global_manifest_template.render(downloadurl = downloadurl ))
 
-for packages in global_manifest_template['packages']:
-    print(packages['sourceURL'])
-    download_plugin = requests.get("{}".format(packages['sourceURL']))
-    print(download_plugin)
+for key in global_manifest_int['packages']:
+    print(key['sourceURL'])
+    plugin_req = requests.get(key['sourceURL'])
+    z = zipfile.ZipFile(io.BytesIO(plugin_req.content))
+    z.extractall()
